e3c91a0fc97dea3911d0d4736b89576ffcf43775
Shreeya Garje <shreeyagarje08@gmail.com>
2026-08-23 12:26:27 +0530

    implemented search against title and author and made changes for consistent api response

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,9 +9,12 @@ module.exports.showAllPosts = async (req, res) => {
   if (title) {
     const posts = await Post.find({
       title: { $regex: title, $options: "i" },
-    });
-    res.json({ posts });
+    }).populate("author", "name -_id");
+    res.json(
+      posts.map((post) => ({ title: post.title, author: post.author.name })),
+    );
   } else if (author) {
+    /* method 1 
     const user = await User.find({ name: { $regex: author, $options: "i" } });
     const userIds = user.map((user) => user._id);
 
@@ -19,33 +22,75 @@ module.exports.showAllPosts = async (req, res) => {
       "author",
       "name -_id",
     );
+    */
+
+    // method 2
+    const posts = await Post.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "authorData",
+        },
+      },
+      {
+        $match: {
+          "authorData.name": {
+            $regex: author,
+            $options: "i",
+          },
+        },
+      },
+      // used unwind in order to have a consistent API response
+      {
+        $unwind: "$authorData",
+      },
+      {
+        $project: {
+          _id: 0,
+          title: 1,
+          author: "$authorData.name",
+        },
+      },
+    ]);
+
     res.json(posts);
+  } else {
+    /*
+    const posts = await Post.find({}).populate("author", "name -_id");
+
+    let result = await Promise.all(
+      posts.map(async (post) => {
+        const comments = await Comment.find({ post: post._id }).populate(
+          "owner",
+          "name -_id",
+        );
+
+        return {
+          title: post.title,
+          content: post.content,
+          author: post.author.name,
+          comments: comments.map((comment) => ({
+            comment: comment.content,
+            owner: comment.owner.name,
+            // no need to show comments over here.
+          })),
+        };
+      }),
+    );
+    */
+
+    const posts = await Post.find({}).populate("author", "name -_id");
+
+    console.log(posts);
+    let result = posts.map((post) => ({
+      title: post.title,
+      author: post.author.name,
+    }));
+
+    res.json(result);
   }
-
-  const posts = await Post.find({}).populate("author", "name -_id");
-
-  let result = await Promise.all(
-    posts.map(async (post) => {
-      const comments = await Comment.find({ post: post._id }).populate(
-        "owner",
-        "name -_id",
-      );
-
-      return {
-        title: post.title,
-        content: post.content,
-        author: post.author.name,
-        comments: comments.map((comment) => ({
-          comment: comment.content,
-          owner: comment.owner.name,
-        })),
-      };
-    }),
-  );
-
-  res.json(result);
-
-  // or use virtuals from mongoose.
 };
 
 module.exports.createPost = async (req, res) => {
